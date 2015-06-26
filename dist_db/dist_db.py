b16b4c0b5159ce16e9565cf8141f41a8e4d611ff
Emily <emily@skrivapa.se>
2015-06-26 15:14:23 +0100

    add replication factor and write consistency

--- dist_db/dist_db.py
+++ dist_db/dist_db.py
@@ -1,8 +1,20 @@
 import node
 
-def write(node_urls, thing):
-    #todo: only works with one node, need to make distributed!
-    node.put_thing(node_urls[0], thing)
+def try_to_write(node_url, thing):
+    try:
+        node.put_thing(node_url, thing)
+        return True
+    except:
+        return False
+
+def write(node_urls,
+          replication_factor,
+          write_consistency,
+          thing):
+    results = [ try_to_write(u, thing) for u in node_urls[:replication_factor] ]
+    success_count = sum(results)
+    if success_count < write_consistency:
+       raise Exception('Only wrote to {0} nodes'.format(success_count))
 
 def read(node_urls, id):
     #todo: only works with one node, need to make distributed!
--- dist_db/main.py
+++ dist_db/main.py
@@ -1,9 +1,11 @@
 import dist_db
 from thing import Thing
 
-node_urls = ['http://localhost:8080']
-dist_db.write(node_urls, Thing(3, 'foo'))
-dist_db.write(node_urls, Thing(7, 'bar'))
+node_urls = ['http://localhost:8080',
+             'http://localhost:8081',
+             'http://localhost:8082']
+dist_db.write(node_urls, 3, 2, Thing(3, 'foo'))
+dist_db.write(node_urls, 3, 2, Thing(7, 'bar'))
 thing_3 = dist_db.read(node_urls, 3)
 thing_7 = dist_db.read(node_urls, 7)
 print thing_3
